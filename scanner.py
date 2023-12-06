9b8c7994cc6a5ac3bf9f33db321057ef1e15f488
xmass6 <144550199+xmass6@users.noreply.github.com>
2023-12-05 21:32:31 -0500

    Delete scanner.py

--- scanner.py
+++
@@ -1,6 +0,0 @@
-import nmap
-import os
-
-nm = nmap.PortScanner()
-nm.scan( "192.168.1.0/24", arguments='-sn -O')
-with open("nmap_results.xml", 'w') as xml_output: xml_output.write(nm.get_nmap_last_output())
